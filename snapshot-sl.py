6a54ae2877325b3d46566a12be6a8a079444af4b
Dominic Lamanna <dlamanna10@gmail.com>
2024-12-10 20:58:54 -0600

    Dec 10th update

--- snapshot-sl.py
+++ snapshot-sl.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import streamlit as st
 import pycountry
-import plotly.express as ps
+import plotly.express as px
 
 st.set_page_config(page_title = 'Snapshot', page_icon = '📊', layout = 'wide')
-st.title('Your Dashboard')
-
 # Upload DistroKid CSV (try and find way to convert from tsv to csv for them)
 uploaded_file = st.file_uploader('Upload your DistroKid data here (convert to csv)', type=['csv'])
 
@@ -35,22 +33,88 @@ def cleaning_process(artist_data):
 
     return artist_data
 
-if uploaded_file:
-    try:
-        # Read the uploaded CSV
-        raw_data = pd.read_csv(uploaded_file)
+# Start of Dashboard Design
 
-        # Validate the uploaded file
-        if raw_data.empty:
+menu = st.sidebar.radio(
+    'Dashboard',
+    options = ['Home', 'Streams', 'Earnings', 'Marketing']
+)
+
+if uploaded_file:
+    # Data processing input
+    raw_data = pd.read_csv(uploaded_file)
+    if raw_data.empty:
             st.error("The uploaded file is empty.")
             st.stop()
+    cad = cleaning_process(raw_data)
 
-        # Apply the cleaning process
-        cad = cleaning_process(raw_data)
+    # Home menu design
+    if menu == 'Home':
+        st.title('At a glance...')
+        c1, c2 = st.columns(2)
+        
+        # Key metrics for home page (at a glance)
+        total_streams = cad['Quantity'].sum()
+        total_earnings = cad['Earnings'].sum()
+        avg_eps = total_earnings/total_streams
 
-        # Display the cleaned data
-        st.subheader('Cleaned Data')
-        st.dataframe(cad)
+        def key_metric_styling(label, value):
+            return f"""
+            <div style="padding: 5px; text-align: left; display: inline-block; width: 100%;">
+                <!-- Label styling -->
+                <div style="color: white; font-size: 32px; font-weight: bold; background-color: transparent; padding: 5px; text-decoration: underline;">
+                    {label}
+                </div>
+                <!-- Value styling -->
+                <div style="align: right; background-color: white; color: black; font-size: 24px; font-weight: bold; text-align: right; padding: 5px; width: 80%;">
+                    {value}
+                </div>
+            </div>
+            """
 
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+        with c1:
+            st.title('Key Metrics')
+            st.markdown(key_metric_styling('Total Streams', f"{total_streams:,}"), unsafe_allow_html=True)
+            st.markdown(key_metric_styling('Total Earnings (USD)', f"${round((total_earnings), 2):,} (USD)"), unsafe_allow_html=True)
+            st.markdown(key_metric_styling('Average Earnings/Stream', f"${round((avg_eps), 5):,} (AES)"), unsafe_allow_html=True)
+    
+        with c2:
+            # Add a radio button for the toggle
+            st.title('International Reach')
+            # Getting all countries for the graph
+            all_countries = px.data.gapminder()[['country']].drop_duplicates()
+            all_countries.columns = ['Country']
+
+            country_streams = cad.groupby('Country')['Quantity'].sum().reset_index()
+            country_streams_all = all_countries.merge(country_streams, on = 'Country', how = 'outer')
+            country_streams_all['Quantity'] = country_streams_all['Quantity'].fillna(0)
+            country_streams_exu = country_streams_all[country_streams_all['Country'] != 'Unknown']
+
+            include_us = st.checkbox('Include US Data', value = False)
+            if not include_us:
+                country_streams_exu = country_streams_exu[country_streams_exu['Country'] != 'United States']
+            fig = px.choropleth(
+                        country_streams_exu, locations = 'Country', locationmode = 'country names', 
+                        color = 'Quantity',
+                        color_continuous_scale= 'gray',
+                        range_color = (0, country_streams_exu['Quantity'].max())
+            )
+
+            fig.update_layout(
+                    geo=dict(
+                        bgcolor = 'black',
+                        landcolor = 'white'
+                    ),
+                    title = None, margin = dict(l = 0, r = 0, t = 0, b = 0),
+                    height = 400
+            )
+            
+            # Allocating for streams with an unknown country of sale
+            unknown_streams = country_streams[country_streams['Country'] == 'Unknown']['Quantity'].sum()
+
+            st.plotly_chart(fig, use_container_width=True)
+            
+
+else:
+    st.info('Please upload your data to begin.')
+
